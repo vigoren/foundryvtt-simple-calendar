cecad72e78793265e3a8f960a669005370e73260
Dean Vigoren <vigorator@gmail.com>
2021-04-22 16:43:02 -0600

    Moved the timeKeepingCheck so that it is run by the primary GM after that GM has taken over the primary GM role. When setting the simple calendar from time, moved the SaveCurrentDate call so that it is only triggered when the change was initiated by a combat round ending. Fixed a bug when clicking on day icons that would throw an error and not select the day. Updated how Simple Calendar checks for active combats. It now only checks on the currently active scene. Changed up the date time controls for the GM they now propagate their changes to all players immediately instead of requiring the update date button to be clicked. This feels more intuitive. The Set Current Date button now only shows up when a day has been selected. Clicking this button will now set the selected day as the current day. If the selected day is not visible a dialog will show up to confirm that is what you wanted. Added a new compact view. To toggle between the compact and full views double click on the dialogs header. Fixed an error where the moon phase icons for first and last quarter were switched. Updated all tests around these changes.

--- __mocks__/application.ts
+++ __mocks__/application.ts
@@ -1,11 +1,18 @@
 
 // @ts-ignore
 class Application {
-    constructor() {}
+    constructor() {
+        this.element = {
+            find: jest.fn().mockReturnValue({show: jest.fn(), hide: jest.fn()})
+        };
+    }
 
     static get defaultOptions() { return {title:'',template:'',resizable: false, classes: []}; }
 
     rendered = true;
+
+    // @ts-ignore
+    element: any;
 
     render(force: boolean, options: any){}
 
