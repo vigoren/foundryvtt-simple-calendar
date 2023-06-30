52a70ab1a94e59893fbd8afd49b872bfe82c9401
Dean Vigoren <vigorator@gmail.com>
2023-06-30 09:26:24 -0600

    DOCS: Redirect fixes

--- simple-calendar-docs/docusaurus.config.js
+++ simple-calendar-docs/docusaurus.config.js
@@ -68,31 +68,33 @@ const config = {
       {
         fromExtensions: ['html', 'htm'],
         redirects: [
-          { to: '/docs/faq', from: '/pages/site/docs/faq' },
+          { to: '/docs/faq', from: ['/pages/site/docs/faq', '/pages/site/docs/faq.html'] },
           { to: '/docs/changelog', from: ['/pages/site/CHANGELOG', '/pages/site/CHANGELOG.html'] },
           //Using SC
-          {to: '/docs/using-sc/changing-date-time', from: '/pages/docs/using-sc/index/changing-date-time' },
-          {to: '/docs/using-sc/switching-calendars', from: '/pages/docs/using-sc/index/switching-calendars' },
-          {to: '/docs/using-sc/themes', from: '/pages/docs/using-sc/index/themes' },
-          {to: '/docs/using-sc/notes/', from: '/pages/docs/using-sc/index/notes' },
-          {to: '/docs/using-sc/notes/adding', from: '/pages/docs/using-sc/index/notes/index/adding-editing-removing'},
+          {to: '/docs/using-sc/changing-date-time', from: ['/pages/docs/using-sc/index/changing-date-time', '/pages/docs/using-sc/index/changing-date-time.html'] },
+          {to: '/docs/using-sc/switching-calendars', from: ['/pages/docs/using-sc/index/switching-calendars', '/pages/docs/using-sc/index/switching-calendars.html'] },
+          {to: '/docs/using-sc/themes', from: ['/pages/docs/using-sc/index/themes', '/pages/docs/using-sc/index/themes.html'] },
+          {to: '/docs/using-sc/notes/', from: ['/pages/docs/using-sc/index/notes', '/pages/docs/using-sc/index/notes.html'] },
+          {to: '/docs/using-sc/notes/adding', from: ['/pages/docs/using-sc/index/notes/index/adding-editing-removing', '/pages/docs/using-sc/index/notes/index/adding-editing-removing.html']},
           //Developing with SC
-          {to: '/docs/developing-with-sc/', from: '/pages/docs/developing-with-sc'},
-          {to: '/docs/developing-with-sc/theming', from: '/pages/docs/developing-with-sc/index/theming'},
+          {to: '/docs/developing-with-sc/', from: ['/pages/docs/developing-with-sc', '/pages/docs/developing-with-sc.html']},
+          {to: '/docs/developing-with-sc/theming', from: ['/pages/docs/developing-with-sc/index/theming', '/pages/docs/developing-with-sc/index/theming.html']},
           //API Docs
-          {to: '/docs/developing-with-sc/api/namespaces/SimpleCalendar', from: '/modules/SimpleCalendar'},
+          {to: '/docs/developing-with-sc/api/namespaces/SimpleCalendar', from: ['/modules/SimpleCalendar', '/modules/SimpleCalendar.html']},
         ],
         createRedirects(path) {
           // Global Config
           if(path.includes('/docs/global-configuration')){
             return [
-                path.replace('/docs/global-configuration', '/pages/global-configuration/index')
+                path.replace('/docs/global-configuration', '/pages/global-configuration/index'),
+                path.replace('/docs/global-configuration', '/pages/global-configuration/index') + '.html'
             ];
           }
           // Calendar Config
           if(path.includes('/docs/calendar-configuration')){
             return [
-              path.replace('/docs/calendar-configuration', '/pages/calendar-configuration/index')
+              path.replace('/docs/calendar-configuration', '/pages/calendar-configuration/index'),
+              path.replace('/docs/calendar-configuration', '/pages/calendar-configuration/index') + '.html'
             ];
           }
           return undefined;
