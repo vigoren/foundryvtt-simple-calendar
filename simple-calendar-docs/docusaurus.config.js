d087efe7d334affd75de62560e6cdc4e19c4c02a
Dean Vigoren <vigorator@gmail.com>
2023-06-30 11:07:12 -0600

    DOCS: Updated the social share image

--- simple-calendar-docs/docusaurus.config.js
+++ simple-calendar-docs/docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo.svg',
+      image: 'img/logo.png',
       navbar: {
         title: 'Simple Calendar',
         logo: {
