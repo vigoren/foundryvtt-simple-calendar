546b705ff71c047baec9ab9ad42cdf4612054a65
Dean Vigoren <vigorator@gmail.com>
2023-09-12 20:48:39 -0600

    TESTS: Updated the unit tests for the new leap year setting "Starting Year"

--- src/classes/calendar/leap-year.test.ts
+++ src/classes/calendar/leap-year.test.ts
@@ -26,7 +26,7 @@ describe("Leap Year Tests", () => {
     });
 
     test("Properties", () => {
-        expect(Object.keys(lr).length).toBe(8); //Make sure no new properties have been added
+        expect(Object.keys(lr).length).toBe(9); //Make sure no new properties have been added
         expect(lr.rule).toBe(LeapYearRules.None);
         expect(lr.customMod).toBe(1);
     });
@@ -40,12 +40,14 @@ describe("Leap Year Tests", () => {
         expect(lyc.id).toBeDefined();
         expect(lyc.rule).toBe(LeapYearRules.None);
         expect(lyc.customMod).toBe(1);
+        expect(lyc.startingYear).toBe(0);
     });
 
     test("To Template", () => {
         const lyt = lr.toTemplate();
         expect(lyt.rule).toBe(LeapYearRules.None);
         expect(lyt.customMod).toBe(1);
+        expect(lyt.startingYear).toBe(0);
     });
 
     test("Load From Settings", () => {
@@ -60,19 +62,29 @@ describe("Leap Year Tests", () => {
         lr.loadFromSettings();
         expect(lr.rule).toBe(LeapYearRules.None);
         expect(lr.customMod).toBe(1);
+        expect(lr.startingYear).toBe(0);
         //@ts-ignore
         lr.loadFromSettings({});
         expect(lr.rule).toBe(LeapYearRules.None);
         expect(lr.customMod).toBe(1);
+        expect(lr.startingYear).toBe(0);
 
         lr.loadFromSettings(config);
         expect(lr.rule).toBe(LeapYearRules.Custom);
         expect(lr.customMod).toBe(10);
+        expect(lr.startingYear).toBe(0);
 
         config.customMod = 0;
         lr.loadFromSettings(config);
         expect(lr.rule).toBe(LeapYearRules.None);
         expect(lr.customMod).toBe(0);
+        expect(lr.startingYear).toBe(0);
+
+        config.startingYear = 1;
+        lr.loadFromSettings(config);
+        expect(lr.rule).toBe(LeapYearRules.None);
+        expect(lr.customMod).toBe(0);
+        expect(lr.startingYear).toBe(1);
     });
 
     test("Is Leap Year", () => {
@@ -95,8 +107,17 @@ describe("Leap Year Tests", () => {
         expect(lr.isLeapYear(5)).toBe(true);
         expect(lr.isLeapYear(8)).toBe(false);
 
+        lr.startingYear = 1;
+        expect(lr.isLeapYear(2021)).toBe(true);
+        expect(lr.isLeapYear(2020)).toBe(false);
+        expect(lr.isLeapYear(0)).toBe(false);
+        expect(lr.isLeapYear(1)).toBe(true);
+        expect(lr.isLeapYear(5)).toBe(false);
+        expect(lr.isLeapYear(6)).toBe(true);
+
         jest.spyOn(FoundryVTTGameData, "systemID", "get").mockReturnValue("pf2e");
         tCal.generalSettings.pf2eSync = true;
+        lr.startingYear = 0;
         //@ts-ignore
         game.pf2e = { worldClock: { dateTheme: "AD", worldCreatedOn: 10000 } };
         expect(lr.isLeapYear(2020)).toBe(true);
@@ -127,12 +148,24 @@ describe("Leap Year Tests", () => {
         expect(lr.howManyLeapYears(6)).toBe(1);
         expect(lr.howManyLeapYears(2020)).toBe(403);
         expect(lr.howManyLeapYears(2021)).toBe(404);
+
+        lr.startingYear = 1;
+        expect(lr.howManyLeapYears(0)).toBe(-1);
+        expect(lr.howManyLeapYears(5)).toBe(0);
+        expect(lr.howManyLeapYears(6)).toBe(0);
+        expect(lr.howManyLeapYears(7)).toBe(1);
+        expect(lr.howManyLeapYears(2020)).toBe(403);
+        expect(lr.howManyLeapYears(2021)).toBe(403);
+        expect(lr.howManyLeapYears(2022)).toBe(404);
     });
 
     test("Previous Leap Year", () => {
         expect(lr.previousLeapYear(1990)).toBe(null);
         lr.rule = LeapYearRules.Gregorian;
         expect(lr.previousLeapYear(1990)).toBe(1988);
+
+        lr.startingYear = 1;
+        expect(lr.previousLeapYear(1990)).toBe(1989);
     });
 
     test("Fraction", () => {
@@ -142,7 +175,9 @@ describe("Leap Year Tests", () => {
         lr.rule = LeapYearRules.Custom;
         lr.customMod = 5;
         expect(lr.fraction(1991)).toBe(0.2);
-
+        lr.startingYear = 1;
+        expect(lr.fraction(1991)).toBe(0.8);
+        expect(lr.fraction(1992)).toBe(0);
         lr.customMod = 0;
         expect(lr.fraction(1990)).toBe(0);
     });
